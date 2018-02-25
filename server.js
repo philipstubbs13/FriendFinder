386e5d3a325a3cf637b971ee06684584b680d7ba
Phil S <philipstubbs13@gmail.com>
2018-02-25 11:24:19 -0600

    successfully set up html routes

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ var path = require("path");
 
 // Sets up the Express App
 var app = express();
-var PORT = process.env.PORT || 1313;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+//require("./app/routing/apiRoutes")(myapp);
+require("./app/routing/htmlRoutes")(app);
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
