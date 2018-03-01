4508b544d6b3632fbdde001f4a53d57c01b57caf
Phil S <philipstubbs13@gmail.com>
2018-03-01 06:07:21 -0600

    figured out how to serve up static files in express #almostdone

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/app')); 
 
 // ROUTER
 // The below points our server to a series of "route" files.
