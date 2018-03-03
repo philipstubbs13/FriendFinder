3f3fc2342e83086cacdde6f50ddcd19a1ae97f60
Phil S <philipstubbs13@gmail.com>
2018-03-03 06:40:35 -0600

    updated score values

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,37 +15,37 @@ var friendsData = [
 	{
 	  "name": "Trae Young",
 	  "photo": "https://cdn.vox-cdn.com/thumbor/xi7t5IWIeoLQPcMKJQiCPzblTqc=/1185x607:4443x3080/1200x800/filters:focal(2339x1411:3229x2301)/cdn.vox-cdn.com/uploads/chorus_image/image/58081117/usa_today_10481466.0.jpg",
-	  "scores": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	  "scores": [25, 25, 25, 25, 25, 25, 25, 25, 25, 25]
 	},
 
 	{
 	  "name": "Grayson Allen",
 	  "photo": "https://img.bleacherreport.net/img/images/photos/003/718/021/hi-res-b730f72c8a3f46d9fc55a4af6a974e03_crop_north.jpg?h=533&w=800&q=70&crop_x=center&crop_y=top",
-	  "scores": [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+	  "scores": [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 	},
 
 	{
 	  "name": "Zach LaVine",
 	  "photo": "https://espngrantland.files.wordpress.com/2014/09/453120688-e1411503677676.jpg?w=2048",
-	  "scores": [2, 2, 2, 2, 2, 4, 4, 4, 4, 4]
+	  "scores": [10, 10, 10, 10, 10, 50, 50, 50, 50, 50]
 	},
 
 	{
 	  "name": "Justin Bieber",
 	  "photo": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Justin_Bieber_in_Rosemont%2C_Illinois_%282015%29.jpg",
-	  "scores": [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+	  "scores": [50, 50, 50, 50, 50, 50, 50, 50, 50, 50]
 	},
 
 	{
 	  "name": "Selena Gomez",
 	  "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Selena_Gomez_-_Walmart_Soundcheck_Concert.jpg/492px-Selena_Gomez_-_Walmart_Soundcheck_Concert.jpg",
-	  "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+	  "scores": [90, 90, 90, 90, 90, 90, 90, 90, 90, 90]
 	},
 
 	{
 	  "name": "Jennifer Lawrence",
 	  "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Jennifer_Lawrence_SDCC_2015_X-Men.jpg/1200px-Jennifer_Lawrence_SDCC_2015_X-Men.jpg",
-	  "scores": [3, 3, 4, 4, 4, 5, 5, 2, 1, 2]
+	  "scores": [25, 25, 50, 50, 50, 90, 90, 10, 1, 10]
 	}
 ];
 
